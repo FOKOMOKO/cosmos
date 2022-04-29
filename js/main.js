84ae8008ecc08d7448e317adfb2fd6e60b2e5f3d
FOKOMOKO <70406682+FOKOMOKO@users.noreply.github.com>
2022-04-29 18:53:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
     slidesToShow: 1, // показывать по 3 слайда
     autoplay: true, // автоматическое проигрывание слайдов
     autoplaySpeed: 1000,
-    prevArrow: '<img class="slick-btn slick-prev" src="/images/left.png" alt="">',
-    nextArrow: '<img class="slick-btn slick-next" src="/images/right.png" alt="">'
+    prevArrow: '<img class="slick-btn slick-prev" src="images/left.png" alt="">',
+    nextArrow: '<img class="slick-btn slick-next" src="images/right.png" alt="">'
   });
   $('.menu__item').on('click', function () {
 $('.header__list').toggleClass('active')
